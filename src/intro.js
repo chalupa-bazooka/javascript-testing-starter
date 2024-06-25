e4f6282be112f6db6eca7dc78a94a220957c3553
chalupa-bazooka <mteplyshev@gmail.com>
2024-06-25 20:59:20 +0400

    Add TTD function

--- src/intro.js
+++ src/intro.js
@@ -1,16 +1,47 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-  a > b ? a : b
-
-  if (a > b) return a;
-  else if (b > a) return b;
-  return a;
+  return a > b ? a : b
 }
 
 // Exercise
-export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
-  return n.toString();
+export function fizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz'
+  } else if (num % 3 === 0) {
+    return "Fizz"
+  } else if (num % 5 === 0) {
+    return "Buzz"
+  } else {
+    return num.toString()
+  }
+}
+
+// TTD intro
+export function calculateAverage(nums) {
+  if (!nums.length) {
+    return NaN
+  } else {
+    const sum = nums.reduce((sum, current) => sum + current, 0)
+    return sum / nums.length
+  }
+}
+
+// TTD intro 2
+export function calculateFactorial(num) {
+  if (isNaN(num)) {
+    return NaN
+  } else if (num < 0) {
+    return "It won't work with negative numbers"
+  } else if (num === 0) {
+    return 1
+    // } else {
+    //   let accum = 1
+    //   for (let i = num; i > 0; i--) { 
+    //     accum = accum * i 
+    //   }
+    //   return accum
+    // }
+  } else {
+    return num * calculateFactorial(num - 1)
+  }
 }
--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,51 +1,107 @@
 import { describe, test, it, expect } from "vitest"
-import { max, fizzBuzz } from "../intro"
+import { max, fizzBuzz, calculateAverage, calculateFactorial } from "../intro"
 
-describe("max", () => {
-    it("should return the 1st argument if it's greater", () => {
-        // AAA: Arrange, Act, & Assert
+// describe("max", () => {
+//     it("should return the 1st argument if it's greater", () => {
+//         // AAA: Arrange, Act, & Assert
 
-        // A1: Arrange
-        const a = 2
-        const b = 1
+//         // A1: Arrange
+//         const a = 2
+//         const b = 1
 
-        // A2: Act
-        const result = max(a, b)
+//         // A2: Act
+//         const result = max(a, b)
 
-        // A3: Assert
-        expect(result).toBe(2)
+//         // A3: Assert
+//         expect(result).toBe(2)
 
-        // you can break your test code into 3 A-parts as above
-        // or you can simplify it, if it's straightforward enough:
-        // expect(max(2, 1)).toBe(2)
-        // – it will work just as well 
+//         // you can break your test code into 3 A-parts as above
+//         // or you can simplify it, if it's straightforward enough:
+//         // expect(max(2, 1)).toBe(2)
+//         // – it will work just as well 
+//     })
+
+//     it("should return the 2nd argument if it's greater", () => {
+//         expect(max(1, 2)).toBe(2)
+//     })
+
+//     it("should return the 1st argument if the arguments are equal", () => {
+//         expect(max(2, 2)).toBe(2)
+//     })
+
+// })
+
+// describe("fizzBuzz", () => {
+//     it("should return Fizz if the argument is only fully divisible by 3", () => {
+//         expect(fizzBuzz(3)).toBe("Fizz")
+//     })
+
+//     it("should return Buzz if the argument is only fully divisible by 5", () => {
+//         expect(fizzBuzz(5)).toBe("Buzz")
+//     })
+
+//     it("should return FizzBuzz if the argument is fully divisible by both 3 and 5", () => {
+//         expect(fizzBuzz(15)).toBe("FizzBuzz")
+//     })
+
+//     it("should return the argument as a string if it's not fully divisible by neither 3 nor 5", () => {
+//         expect(fizzBuzz(2)).toBe("2")
+//     })
+
+// })
+
+// describe("calculateAverage", () => {
+//     it("should return NaN if given an empty array", () => {
+//         expect(calculateAverage([])).toBe(NaN)
+//     })
+
+//     it("should calculate the average of an array with 1 item", () => {
+//         expect(calculateAverage([1])).toBe(1)
+//     })
+
+//     it("should calculate the average of an array with 2 items", () => {
+//         expect(calculateAverage([1, 2])).toBe(1.5)
+//     })
+
+//     it("should calculate the average of an array with 3 items", () => {
+//         expect(calculateAverage([1, 2, 3])).toBe(2)
+//     })
+
+//     it("should calculate the average of an array with 10 items", () => {
+//         expect(calculateAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5.5)
+//     })
+// })
+
+describe("calculateFactorial", () => {
+    it("should return NaN if the argument is absent", () => {
+        expect(calculateFactorial()).toBe(NaN)
+    })
+    
+    it("should return NaN if the argument isn't a number", () => {
+        expect(calculateFactorial(["a", 1])).toBe(NaN)
     })
 
-    it("should return the 2nd argument if it's greater", () => {
-        expect(max(1, 2)).toBe(2)
+    it("should return 1 if the argument is 0", () => {
+        expect(calculateFactorial(0)).toBe(1)
     })
 
-    it("should return the 1st argument if the arguments are equal", () => {
-        expect(max(2, 2)).toBe(2)
+    it("should return 1 if the argument is 1", () => {
+        expect(calculateFactorial(1)).toBe(1)
     })
 
-})
-
-describe("fizzBuzz", () => {
-    it("should return Fizz if the argument is only fully divisible by 3", () => {
-        expect(fizzBuzz(3)).toBe("Fizz")
+    it("should return 2 if the argument is 2", () => {
+        expect(calculateFactorial(2)).toBe(2)
     })
 
-    it("should return Buzz if the argument is only fully divisible by 5", () => {
-        expect(fizzBuzz(5)).toBe("Buzz")
+    it("should return 6 if the argument is 3", () => {
+        expect(calculateFactorial(3)).toBe(6)
     })
 
-    it("should return FizzBuzz if the argument is fully divisible by both 3 and 5", () => {
-        expect(fizzBuzz(15)).toBe("FizzBuzz")
+    it("should return 3'628'800 if the argument is 10", () => {
+        expect(calculateFactorial(10)).toBe(3628800)
     })
 
-    it("should return the argument as a string if it's not fully divisible by neither 3 nor 5", () => {
-        expect(fizzBuzz(2)).toBe("2")
+    it("should return 'It won't work with negative numbers' if the argument is negative", () => {
+        expect(calculateFactorial(-1)).toBe("It won't work with negative numbers")
     })
-
 })
