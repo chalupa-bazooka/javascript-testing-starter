1a63bfec36dfa150f757412634b63e20249a83a5
chalupa-bazooka <mteplyshev@gmail.com>
2024-06-29 21:49:46 +0400

    Add more conditionals to validateUserInput

--- src/core.js
+++ src/core.js
@@ -33,11 +33,11 @@ export function calculateDiscount(price, discountCode) {
 export function validateUserInput(username, age) {
   let errors = [];
 
-  if (typeof username !== 'string' || username.length < 3) {
+  if (typeof username !== 'string' || username.length < 3 || username.length > 255) {
     errors.push('Invalid username');
   }
 
-  if (typeof age !== 'number' || age < 18) {
+  if (typeof age !== 'number' || age < 18 || age > 200) {
     errors.push('Invalid age');
   }
 
