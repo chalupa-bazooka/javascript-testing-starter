c0d3584c95f53104c31bf5a96623e0137f088d91
chalupa-bazooka <mteplyshev@gmail.com>
2024-06-29 21:41:10 +0400

    Addpositive & negative testing exercise tests

--- src/tests/core.test.js
+++ src/tests/core.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest"
-import { getCoupons, calculateDiscount } from "../core"
+import { getCoupons, calculateDiscount, validateUserInput } from "../core"
 
 
 // describe("getCoupons", () => {
@@ -38,27 +38,51 @@ import { getCoupons, calculateDiscount } from "../core"
 // })
 
 
-describe("calculateDiscount", () => {
-    it("should return discounted price if given a valid code", () => {
-        expect(calculateDiscount(10, "SAVE10")).toBe(9)
-        expect(calculateDiscount(10, "SAVE20")).toBe(8)
+// describe("calculateDiscount", () => {
+//     it("should return discounted price if given a valid code", () => {
+//         expect(calculateDiscount(10, "SAVE10")).toBe(9)
+//         expect(calculateDiscount(10, "SAVE20")).toBe(8)
 
+//     })
+
+//     it("should handle a non-numeric price", () => {
+//         expect(calculateDiscount("10", "SAVE10")).toMatch(/invalid/i)
+//     })
+
+//     it("should handle a negative rice", () => {
+//         expect(calculateDiscount(-1, "SAVE10")).toMatch(/invalid/i)
+//     })
+
+//     it("should handle a non-string discount code", () => {
+//         expect(calculateDiscount(10, 10)).toMatch(/invalid/i)
+//     })
+
+//     it("should handle a non-existent discount code", () => {
+//         expect(calculateDiscount(10, "INVALID")).toBe(10)
+//     })
+
+// })
+
+
+describe("validateUserInput", () => {
+
+    it("should return 'Validation successful' if there are no errors", () => {
+        expect(validateUserInput("goga", 19)).toMatch(/successful/)
     })
 
-    it("should handle a non-numeric price", () => {
-        expect(calculateDiscount("10", "SAVE10")).toMatch(/invalid/i)
+    it("should return 'Invalid username' if the username is invalid", () => {
+        expect(validateUserInput("go", 19)).toMatch(/invalid username/i)
+        expect(validateUserInput(19, 19)).toMatch(/invalid username/i)
     })
 
-    it("should handle a negative rice", () => {
-        expect(calculateDiscount(-1, "SAVE10")).toMatch(/invalid/i)
+    it("should return 'Invalid age' if the age is invalid", () => {
+        expect(validateUserInput("goga", 17)).toMatch(/invalid age/i)
+        expect(validateUserInput("goga", "19")).toMatch(/invalid age/i)
     })
 
-    it("should handle a non-string discount code", () => {
-        expect(calculateDiscount(10, 10)).toMatch(/invalid/i)
+    it("should return 'Invalid username, Invalid age' if both the username & age are invalid", () => {
+        expect(validateUserInput("go", 17)).toMatch(/invalid username, invalid age/i)
     })
 
-    it("should handle a non-existent discount code", () => {
-        expect(calculateDiscount(10, "INVALID")).toBe(10)
-    })
 
 })
