563f1629736246c3b103ee58f1aa21152a754929
chalupa-bazooka <mteplyshev@gmail.com>
2024-06-30 17:05:16 +0400

    Update tests to be parameterized

--- src/tests/core.test.js
+++ src/tests/core.test.js
@@ -4,94 +4,71 @@ import {
     canDrive,
 } from "../core"
 
-describe("canDrive", () => {
+// describe("canDrive", () => {
 
-    it("should return 'Invalid country code' if an invalid country code has been passed", () => {
-        expect(canDrive(16, "US")).toMatch(/invalid/i)
+//     it("should return 'Invalid country code' if an invalid country code has been passed", () => {
+//         expect(canDrive(16, "US")).toMatch(/invalid/i)
+//     })
+
+//     it("should return 'Age must be a number' if the age isn't a number", () => {
+//         expect(canDrive("10", "GEO")).toMatch(/number/i)
+//         expect(canDrive(null, "GEO")).toMatch(/number/i)
+//     })
+
+//     it("should return 'Pass 2 arguments: age & code' if another number of arguments has been passed", () => {
+//         expect(canDrive()).toMatch(/2 arguments/i)
+//         expect(canDrive(10, "GEO", 12)).toMatch(/2 arguments/i)
+//         expect(canDrive(10)).toMatch(/2 arguments/i)
+//     })
+
+//     it.each([
+//         {age: 16, countryCode: "GBR", result: false},
+//         {age: 17, countryCode: "GBR", result: true},
+//         {age: 18, countryCode: "GBR", result: true},
+//         {age: 16, countryCode: "GEO", result: false},
+//         {age: 17, countryCode: "GEO", result: true},
+//         {age: 18, countryCode: "GEO", result: true},
+//         {age: 14, countryCode: "MEX", result: false},
+//         {age: 15, countryCode: "MEX", result: true},
+//         {age: 16, countryCode: "MEX", result: true},
+//         {age: 17, countryCode: "RUS", result: false},
+//         {age: 18, countryCode: "RUS", result: true},
+//         {age: 19, countryCode: "RUS", result: true},
+//         {age: 15, countryCode: "USA", result: false},
+//         {age: 16, countryCode: "USA", result: true},
+//         {age: 17, countryCode: "USA", result: true},
+//     ])("should return $result when ($age, $countryCode) have been passed", ({age, countryCode, result}) => {
+//         expect(canDrive(age, countryCode)).toBe(result)
+//     })
+
+// })
+
+
+describe("isValidUsername", () => {
+
+    const minLength = 5
+    const maxLength = 15
+
+    it.each([
+        {username: "a".repeat(minLength-1), result: false},
+        {username: "a".repeat(maxLength+1), result: false},
+        {username: "a".repeat(minLength), result: true},
+        {username: "a".repeat(maxLength), result: true},
+        {username: "a".repeat(minLength+1), result: true},
+        {username: "a".repeat(maxLength-1), result: true},
+    ])("should return $result if the username's length is $username.length", ({username, result}) => {
+        expect(isValidUsername(username)).toBe(result)
     })
 
-    it("should return false if the passed age is below the min boundary for the specified country", () => {
-        expect(canDrive(16, "GEO")).toBe(false)
-        expect(canDrive(17, "RUS")).toBe(false)
-    })
-
-    it("should return true if the passed age is at the min boundary for the specified country", () => {
-        expect(canDrive(17, "GEO")).toBe(true)
-        expect(canDrive(18, "RUS")).toBe(true)
-    })
-
-    it("should return true if the passed age is above the min boundary for the specified country", () => {
-        expect(canDrive(18, "GEO")).toBe(true)
-        expect(canDrive(19, "RUS")).toBe(true)
-    })
-
-    it("should return 'Age must be a number' if the age isn't a number", () => {
-        expect(canDrive("10", "GEO")).toMatch(/number/i)
-        expect(canDrive(null, "GEO")).toMatch(/number/i)
-    })
-
-    it("should return 'Pass 2 arguments: age & code' if another number of arguments has been passed", () => {
-        expect(canDrive()).toMatch(/2 arguments/i)
-        expect(canDrive(10, "GEO", 12)).toMatch(/2 arguments/i)
-        expect(canDrive(10)).toMatch(/2 arguments/i)
+    it("should return false for invalid username types", () => {
+        expect(isValidUsername(null)).toBe(false)
+        expect(isValidUsername()).toBe(false)
+        expect(isValidUsername(undefined)).toBe(false)
+        expect(isValidUsername(1)).toBe(false)
 
     })
 
 })
-
-
-// export function canDrive(age, countryCode) {
-//     const legalDrivingAge = {
-//       USA: 16,
-//       GBR: 17,
-//       MEX: 15,
-//       GEO: 17,
-//       RUS: 18,
-//     };
-  
-//     if (!legalDrivingAge[countryCode]) {
-//       return 'Invalid country code';
-//     }
-  
-//     return age >= legalDrivingAge[countryCode];
-//   }
-
-
-
-// describe("isValidUsername", () => {
-
-//     const minLength = 5
-//     const maxLength = 15
-
-//     it("should return false if the username is shorther than the lower boundary", () => {
-//         expect(isValidUsername("a".repeat(minLength-1))).toBe(false)
-//     })
-
-//     it("should return false if the username is longer than the higher boundary", () => {
-//         expect(isValidUsername("a".repeat(maxLength+1))).toBe(false)
-//     })
-
-//     it("should return true if the username's length is equal to either boundary", () => {
-//         expect(isValidUsername("a".repeat(minLength))).toBe(true)
-//         expect(isValidUsername("a".repeat(maxLength))).toBe(true)
-
-//     })
-
-//     it("should return true if the username's length is within the boundaries", () => {
-//         expect(isValidUsername("a".repeat(minLength+1))).toBe(true)
-//         expect(isValidUsername("a".repeat(maxLength-1))).toBe(true)
-
-//     })
-
-//     it("should return false for invalid username types", () => {
-//         expect(isValidUsername(null)).toBe(false)
-//         expect(isValidUsername()).toBe(false)
-//         expect(isValidUsername(undefined)).toBe(false)
-//         expect(isValidUsername(1)).toBe(false)
-
-//     })
-
-// })
 
 
 
